21374a95f83ae74b02a872302f2844d1c76323ef
FrancoL <francovalentin5005@gmail.com>
2023-05-01 22:46:52 -0300

    add configuration

--- src/lib.rs
+++ src/lib.rs
@@ -1,47 +1,62 @@
-use std::fs::{self, OpenOptions};
-use std::io::prelude::*;
 use crate::logs::output_error;
 use data_processor::CsvParserBuilder;
 use files::FileBuilder;
+use std::fs::{self, OpenOptions};
+use std::io::prelude::*;
 
 mod data_processor;
 mod files;
 mod logs;
 
-
-pub fn execute() -> Result<(), std::io::Error> {
+pub fn execute(csv_file: Option<&str>) -> Result<(), std::io::Error> {
     let config_file = FileBuilder::new(".").add_data("config.txt").build();
-    let output = format!("{}/{}",config_file.data(),"result.txt");
-    
+    let output = format!("{}/{}", config_file.data(), "result.txt");
 
     let dir_path = "archivos";
 
-    let files = match file_finder(dir_path) {
-        Ok(value) => value,
-        Err(err) => {
-            output_error(err, &format!("Direccion de la carpeta:{}", dir_path));
-            panic!("Falla en encontrar la carpeta");
+    match csv_file {
+        Some(csv) => {
+            let file = FileBuilder::new(dir_path).add_data(&csv).build();
+
+            let mut data_lines = file.data().lines();
+
+            let columns = &data_lines.next().unwrap().replace("\"", "");
+            let parser = CsvParserBuilder::new()
+                .set_columns(&columns)
+                .data(data_lines.collect())
+                .build();
+
+            let parsed = parser.parse();
+            write_to(&*parsed, &output)?;
+            return Ok(())
         }
-    };
-
-    for file_name in files.iter() {
-        let file = FileBuilder::new(dir_path).add_data(&file_name).build();
-
-
-        let mut data_lines = file.data().lines();
-
-        let columns = &data_lines.next().unwrap().replace("\"", "");
-        let parser = CsvParserBuilder::new()
-            .set_columns(&columns)
-            .data(data_lines.collect())
-            .build();
-
-        let parsed = parser.parse();
-        write_to(&*parsed, &output)?
-
+        None => {
+                let files = match file_finder(dir_path) {
+                    Ok(value) => value,
+                    Err(err) => {
+                        output_error(err, &format!("Direccion de la carpeta:{}", dir_path));
+                        panic!("Falla en encontrar la carpeta");
+                    }
+                };
+            
+                for file_name in files.iter() {
+                    let file = FileBuilder::new(dir_path).add_data(&file_name).build();
+            
+                    let mut data_lines = file.data().lines();
+            
+                    let columns = &data_lines.next().unwrap().replace("\"", "");
+                    let parser = CsvParserBuilder::new()
+                        .set_columns(&columns)
+                        .data(data_lines.collect())
+                        .build();
+            
+                    let parsed = parser.parse();
+                    write_to(&*parsed, &output)?
+                }
+            
+                return Ok(())
+        }
     }
-
-    Ok(())
 }
 
 pub fn file_finder(dir_path: &str) -> Result<Vec<String>, std::io::Error> {
@@ -59,13 +74,13 @@ pub fn file_finder(dir_path: &str) -> Result<Vec<String>, std::io::Error> {
     Ok(result)
 }
 
-fn write_to<'a>(data : impl Into<&'a str>,path: &str)-> Result<(), std::io::Error>{
-    let data : &str  = data.into();
+fn write_to<'a>(data: impl Into<&'a str>, path: &str) -> Result<(), std::io::Error> {
+    let data: &str = data.into();
     let mut file = OpenOptions::new()
-            .write(true)
-            .append(true)
-            .create(true)
-            .open(path)
-            .unwrap();
-        writeln!(file, "{}", data)
+        .write(true)
+        .append(true)
+        .create(true)
+        .open(path)
+        .unwrap();
+    writeln!(file, "{}", data)
 }
